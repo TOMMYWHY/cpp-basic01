7e6b91dddf8182b1a4f096b7f068b18256aec5ec
tommy <tommywhy1989@gmail.com>
2019-10-10 20:15:34 +1300

    Constant pointer vs Pointer to constant

---
+++ const/mian.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int a =10;
+    int b =100;
+    const int * p = &a; // 常量指针 // const 标记 *,则 *p 固定。
+
+//    *p = 11; // error : read only
+//    p = &b; //指向 b
+    cout << "pointer : p => " <<p << endl;
+    cout << "pointer : * p => " <<*p << endl;
+
+
+    int * const pp = &a; // 指针常量 // const 标记 pp，则 pp 固定。
+    *pp = b;
+//    pp =&b; // error:
+    cout << "pointer : pp => " <<pp << endl;
+    cout << "pointer : * pp => " <<*pp << endl;
+
+    cout << "pointer : p => " <<p << endl;
+    cout << "pointer : * p => " <<*p << endl;
+    return 0;
+}
